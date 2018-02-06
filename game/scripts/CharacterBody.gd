d9a0f0e6573fc64f2a6f268a339625baeb121152
sdfgeoff <sdfgeoff@gmail.com>
2018-02-06 12:56:23 +0100

    Fixed mouse osscilation

--- game/scripts/CharacterBody.gd
+++ game/scripts/CharacterBody.gd
@@ -2,9 +2,7 @@ extends RigidBody2D
 
 var _velocity = Vector2(0, 0)
 
-export var min_softness = 0.05
-export var max_speed = 8000
-export var SPEED = 4000
+var min_softness = 0.05
 export(NodePath) var hammer_joint = null
 
 var MOUSE = true
@@ -28,17 +26,24 @@ func set_mouse(val):
 
 func _process(delta):
 	if MOUSE:
-		var vector = get_global_mouse_position() - self.get_global_position()
-		_velocity = vector * SPEED * delta
+		var dir_vect = get_global_mouse_position() - self.get_global_position()
+		_velocity = dir_vect * 0.001
 	elif JOYSTICK:
 		var x_axis = Input.get_joy_axis(0,0)
 		var y_axis = Input.get_joy_axis(0,1)
-		_velocity = Vector2(x_axis, y_axis) * SPEED * delta * 150
-	if abs(_velocity.x) > max_speed or abs(_velocity.y) > max_speed:
+		_velocity = Vector2(x_axis, y_axis)
+	
+	_velocity *= pow(_velocity.length(), 1.0)
+	if _velocity.length() > 1:
 		_velocity = _velocity.normalized()
-		_velocity *= max_speed
+	
+	_velocity *= 500000
+	
+	#if abs(_velocity.x) > max_speed or abs(_velocity.y) > max_speed:
+	#	_velocity = 
+	#	_velocity *= max_speed
 	if not stunned:
-		set_linear_velocity(_velocity)
+			set_linear_velocity(_velocity * delta)
 
 func _input(event):
 	if MOUSE:
