f8fc056df82fe8ed513e9bcddff37a4db0d4da28
Federico Dolce <psykopear@gmail.com>
2018-02-06 13:33:02 +0100

    Level

--- game/scripts/Character.gd
+++ game/scripts/Character.gd
@@ -4,3 +4,7 @@ export var MOUSE = true
 
 func _ready():
 	get_node("./Body").set_mouse(MOUSE)
+	$Hammer/weap00/player.play("rotate")
+	$Hammer/weap00/weap03/player.play("rotate")
+	$Hammer/weap00/weap02/player.play("rotate")
+	$Hammer/weap00/weap01/player.play("rotate")
