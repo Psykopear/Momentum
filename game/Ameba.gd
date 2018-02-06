46ec3c5f38cddfe44ad8f1a8d041d8a750a1ec82
sdfgeoff <sdfgeoff@gmail.com>
2018-02-06 13:31:22 +0100

    Ameba now dies

--- game/Ameba.gd
+++ game/Ameba.gd
@@ -9,11 +9,13 @@ export (float) var ACCELERATION = 0.02
 
 var target = null
 var rand_velocity = Vector2(0, 0)
+var hitpoints = 2
+
+var amebadeath = load('res://AmebaDeath.tscn')
 
 func _ready():
 	self.target = get_player()
 	randomize()
-	print("Here")
 
 func _process(delta):
 	# Called every frame. Delta is time since last frame.
@@ -32,7 +34,16 @@ func _process(delta):
 	force += (to_target - self.linear_velocity) * ACCELERATION  # Proportional controller
 	self.apply_impulse(Vector2(), force * self.mass)
 	
-
+func _on_Ameba_body_entered(obj):
+	self.hitpoints -= 1
+	print(self.hitpoints)
+	if self.hitpoints < 0:
+		var death = self.amebadeath.instance()
+		self.get_tree().get_root().add_child(death)
+		death.position = self.position
+		queue_free()
+		
+	
 
 func get_player():
 	return get_tree().get_root().get_node('Main/Player/Body')
